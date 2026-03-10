c45d168525fda77f9ed551f804b9abf1ed0c291d
miggyfox <galanmiguel932@gmail.com>
2026-03-10 14:55:23 +1100

    create.js
    for burger animation

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
